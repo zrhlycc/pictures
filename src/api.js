e0cadf7e6fac65ff043eab0951c3b4ac458ab731
zrhlycc <azrahelycc71006@gmail.com>
2024-04-18 16:30:49 -0700

    Cambios en api.js y en ImageList.jsx

--- src/api.js
+++ src/api.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 
 const searchImage = async(term) => {
-    const url = ''
+    const url = 'http://api.unsplash.com/search/photos/?client_id=G25PBI_4eMlPgyyen3VPQ5YLE0sifGhhzoOOQVa368g'
     const response = await axios.get(url, {
         params: {
             query: 'cars'
         }
     })
 
-    console.log(response)
+    return response.data.results
+
 }
+
+export default searchImage
