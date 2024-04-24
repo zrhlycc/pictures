7adf5258fbb583f5cf5c45475298674265947c7a
zrhlycc <azrahelycc71006@gmail.com>
2024-04-24 13:51:31 -0700

    Modificaciones en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import SearchBar from './components/SearchBar'
+import ImageList from './components/ImageList'
+import searchImage from './api'
 import './App.css'
 
 function App() {
@@ -7,12 +9,20 @@ function App() {
 
   const handleSubmit = async(term) => {
     console.log('Usted está buscando con: ', term)
+    const result = await searchImage(term)
+
+    console.log('coco')
+    console.log(result)
+
+    setImages(result)
+    
   }
 
   return (
     <div>
       <h1>App</h1>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images}/>
     </div>
   )
 }
